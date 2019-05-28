543b3134e131ad029391d19d322c0c7cca4215a7
Hsuan Lee <hsuangm@gmail.com>
2019-05-28 20:38:03 +0800

    feat: add layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NzIconService } from 'ng-zorro-antd'
 import { MessageListener } from './core/message-listeners'
 import { MessageListenersManager } from './core/message-listeners-manager'
 import { Receive, Send } from './interfaces/message'
 import { MessageService } from './services/message.service'
+
+const planeSvg = `<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg t="1559045085399" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3854" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200"><defs><style type="text/css"></style></defs><path d="M998.976 554.3232C1031.232 539.6032 1031.328 515.7952 998.976 501.0432L122.88 101.3312C90.624 86.6112 64.448 103.5072 64.384 138.4832L64 426.9952 773.568 527.6672 64 628.3392 64.384 916.8832C64.448 952.1152 90.528 968.7872 122.88 954.0352L998.976 554.3232Z" p-id="3855"></path></svg>`
 
 @Component({
   selector: 'app-root',
@@ -10,10 +13,11 @@ import { MessageService } from './services/message.service'
   styleUrls: ['./app.component.less']
 })
 export class AppComponent extends MessageListenersManager implements OnInit {
-  title = 'ts-websocket';
-
-  constructor(public messageService: MessageService) {
+  isCollapsed = false;
+  username = 'Username'
+  constructor(public messageService: MessageService, private iconService: NzIconService) {
     super(messageService);
+    this.iconService.addIconLiteral('icon:plane', planeSvg);
     this.messageService.connect();
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
